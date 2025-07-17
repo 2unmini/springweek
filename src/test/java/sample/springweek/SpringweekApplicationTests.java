1e73ce32ad1127813f9eca4800e1d98134e2a955
2unmi <2unmin4030@gmail.com>
2025-07-17 14:13:49 +0900

    init 프로젝트 세팅

---
+++ src/test/java/sample/springweek/SpringweekApplicationTests.java
@@ -0,0 +1,13 @@
+package sample.springweek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringweekApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
