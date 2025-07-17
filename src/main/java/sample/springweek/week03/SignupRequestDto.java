df306d3d85fe3ca8d2a51cc0df66fb3a690c1252
2unmi <2unmin4030@gmail.com>
2025-07-17 14:52:46 +0900

    feat Bean Validation의 애너테이션 @Pattern을 사용해서 회원 가입 Email 데이터 검증을 진행합니다.

---
+++ src/main/java/sample/springweek/week03/SignupRequestDto.java
@@ -0,0 +1,19 @@
+package sample.springweek.week03;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SignupRequestDto {
+
+    @NotBlank(message = "이메일은 필수 입력값입니다.")
+    @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$"
+            , message = "올바른 이메일 형식이 아닙니다.")
+    private String email;
+
+}
